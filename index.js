5a6149b090002d3b6f15c1861d15bd4c3ecdda29
Ryan Graham <r.m.graham@gmail.com>
2014-09-23 17:29:29 -0700

    Improve formatting of assertion failures
    Not quite as fancy as chai, but better than vanilla and TAP compliant.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var fmt = require('util').format;
 
 var tapsert = module.exports = tapifyAssert(assert);
 
@@ -23,8 +24,30 @@ function tapifyAssert(assert) {
       console.log('ok %d - %s', n, desc);
     } catch (e) {
       failures += 1;
-      console.log('not ok %d - %s', n, desc)
+      console.log('not ok %d - %s', n, desc);
+      console.log(formatAssertionError(e));
     }
+  }
+}
+
+function formatAssertionError(e) {
+  error = [
+    fmt('actual: %j', e.actual),
+    fmt('expected: %j', e.expected),
+    fmt('operator: %j', e.operator),
+    fmt('message: %s', e.message),
+    e.stack,
+  ].join('\n');
+  return prefixLines('# ', error);
+}
+
+function prefixLines(prefix, text) {
+  return text.split('\n')
+             .map(prepend)
+             .join('\n');
+
+  function prepend(line) {
+    return prefix + line;
   }
 }
 
